c177c31a71569f6aa94b3d0944d969065c59d828
Dajana Stojanović <daja15@live.com>
2015-09-30 23:22:14 +0200

    gotova formaPrijava.php

--- user/formaPrijava.php
+++ user/formaPrijava.php
@@ -15,12 +15,14 @@
 <form action="<?php echo $put;?>user/prijava.php" method="POST">
 	<input class="log" type="text" id="email" name="email" placeholder="korisničko ime ili email" /><br />
 	<input class="log" type="password" id="lozinka" name="lozinka" placeholder="lozinka" /><br />
-	<button type="submit" id="prijavi"> Prijavi </button>
+	<button class="btn1 btn-default" id="prijavi" type="submit" style="margin-top:10px;margin-bottom:20px"> Prijavi </button><br />
+	<a href="" style="text-decoration:underline;color:black;">Zaboravili ste lozinku?</a>
 </form>
 </div>
-
-
-
+<div class="registriraj">
+Niste registrirani? <br/>
+<button class="btn1 btn-default" id="prijavi" type="submit" style="margin-top:15px;margin-bottom:20px;"><a href="<?php echo $put ?>user/formaRegistracija.php" style="color:#d6d6d5;text-decoration:none;">Sign up!</a></button>
+</div>
 
 
 <?php 	
